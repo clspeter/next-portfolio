b965fb64fe2321e77b63668b5a1b52066cc205bc
mustpe <mustpe@gmail.com>
2023-04-30 01:54:28 +0800

    add my image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import Image from "next/image";
 
 type Props = {};
 
@@ -8,19 +9,31 @@ function Hero({}: Props) {
   const [text, count] = useTypewriter({
     words: [
       "Hi, The Name's Peter Liu.",
-      "Im_a_software_engineer.tsx",
+      "Guy-who-loves-tea.tsx",
       "CodingIsWhatILoveToDo.tsx",
     ],
     loop: true,
     deleteSpeed: 50,
   });
   return (
-    <div className="h-screen flex flex-col -space-y-16 md:space-y-8 items-center justify-center">
+    <div className="h-screen flex flex-col -space-y-16 md:space-y-8 items-center justify-center text-center">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <div>
+        <Image
+          src="/peter.png"
+          alt=""
+          className="relative rounded-full h-32 w-32 mx-auto object-cover"
+          width={128}
+          height={128}
+        />
+        <h2 className="text-sm uppercase text-gray-500 md:tracking-[15px] tracking-[5px] mt-4">
+          Software Engineer
+        </h2>
+        <h1 className="mt-2 md:mt-4">
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+      </div>
     </div>
   );
 }
