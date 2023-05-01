96c94c1f8904844cd0ac81490938a190017224f1
clspeter <mustpe@gmail.com>
2023-05-01 20:26:02 +0800

    skills page done

---
+++ components/Skill.tsx
@@ -0,0 +1,32 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+type Props = {
+  directionTop: boolean;
+};
+
+const Skill = (props: Props) => {
+  return (
+    <div className="group relative flex cursor-pointer">
+      <motion.img
+        initial={{
+          y: props.directionTop ? -200 : 200,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        whileInView={{ y: 0, opacity: 1 }}
+        src="js.png"
+        className=" h-24 w-24 rounded-full border border-gray-500 object-cover filter transition duration-200 ease-in-out group-hover:grayscale xl:h-32 xl:w-32"
+      ></motion.img>
+      <div className="absolute h-24 w-24 rounded-full opacity-0 transition duration-300 ease-in-out group-hover:bg-white group-hover:opacity-80 xl:h-32 xl:w-32">
+        <div className="flex h-full items-center justify-center">
+          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skill;
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Skill from "./Skill";
 
 type Props = {};
 
@@ -8,15 +9,30 @@ const Skills = (props: Props) => {
     <motion.div
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
-      transition={{ duration: 1.5 }}
+      transition={{ duration: 1 }}
       className="md:test-left max-w-2000px relative mx-auto flex h-screen min-h-screen flex-col items-center justify-center text-center xl:flex-row xl:px-10"
     >
-      <h3 className="absolute top-24  text-2xl uppercase tracking-[20px] text-gray-500">
+      <h3 className="absolute top-24 text-2xl uppercase tracking-[20px] text-gray-500">
         Skills
         <p className="mt-4 text-sm tracking-[3px]">
           Hover over a skill for currency proficiency
         </p>
       </h3>
+
+      <div className="grid grid-cols-4 gap-5">
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+      </div>
     </motion.div>
   );
 };
