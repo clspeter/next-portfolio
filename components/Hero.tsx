b9ef7f8bbf451164fe43744b54698934070b2b8a
clspeter <mustpe@gmail.com>
2023-05-01 15:35:48 +0800

    add navigate buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {};
 
@@ -26,13 +27,28 @@ function Hero({}: Props) {
           width={128}
           height={128}
         />
-        <h2 className="text-sm uppercase text-gray-500 md:tracking-[15px] tracking-[5px] mt-4">
+        <h2 className="text-md uppercase text-gray-500 md:tracking-[15px] tracking-[5px] mt-4">
           Software Engineer
         </h2>
-        <h1 className="mt-2 md:mt-4 z-10">
+        <h1 className="mt-2 md:mt-4 z-10 font-semibold text-3xl md:text-5xl">
           <span>{text}</span>
           <Cursor cursorColor="#F7AB0A" />
         </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
