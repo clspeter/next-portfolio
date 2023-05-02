2c257b0641e8874f066710186a807cf49d7c0367
clspeter <mustpe@gmail.com>
2023-05-03 00:31:53 +0800

    add types

--- pages/api/getSocials.ts
+++ pages/api/getSocials.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { groq } from 'next-sanity'
 import { sanityClient } from '../../sanity'
+import { Social } from "@/typing";
 
 const query = groq`*[_type == "socials"]`
 
---
+++ typing.d.ts
@@ -0,0 +1,53 @@
+interface SanityBody {
+    _createdAt: string;
+    _id: string;
+    _rev: string;
+    _updatedAt: string;
+}
+
+export interface PageInfo extends SanityBody {
+    _type: "pageInfo";
+    address: string;
+    backgroundInformation: string;
+    email: string;
+    name: string;
+    phoneNumber: string;
+    profilePic: Image;
+    role: string;
+    heroImage: Image;
+}
+export interface Technology extends SanityBody {
+    _type: "skill";
+    title: string;
+    image: Image;
+    progress: number;
+}
+
+export interface Experience extends SanityBody {
+    _type: "experience";
+    company: string;
+    companyImage: Image;
+    dateStarted: date;
+    dateEnded: date;
+    isCurrentlyWorkingHere: boolean;
+    jobTitle: string;
+    points: string[];
+    technologies: Technology[];
+}
+
+export interface Project extends SanityBody {
+    _type: "project";
+    title: string;
+    image: Image;
+    linkToBuild: string;
+    summary: string;
+    technologies: Technology[];
+}
+
+
+
+export interface Social extends SanityBody {
+    _type: "socials";
+    title: string;
+    url: string;
+}
