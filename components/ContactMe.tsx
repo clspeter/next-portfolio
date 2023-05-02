50d0bfb30e2005ecf7e2451f60a94e5c8c7758ef
clspeter <mustpe@gmail.com>
2023-05-02 15:07:43 +0800

    contactme page 2

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -13,12 +13,12 @@ const ContactMe = (props: Props) => {
       <div className="flex flex-col space-y-10">
         <h4 className="font-sejmibold text-center text-4xl">
           I have got just what you need.{" "}
-          <span className="underline decoration-[#F7AB0A]/50">
+          <span className="underline decoration-blue-500/50">
             Let&#39;s Talk
           </span>
         </h4>
 
-        <div className="space-y-10">
+        <div className="space-y-5">
           <div className="flex items-center justify-center space-x-5">
             <PhoneIcon className="h-7 w-7 animate-pulse" />
             <p>+17327924341</p>
@@ -27,7 +27,29 @@ const ContactMe = (props: Props) => {
             <EnvelopeIcon className="h-7 w-7 animate-pulse" />
             <p>mustpe@gmail.com</p>
           </div>
+          <div className="flex items-center justify-center space-x-5">
+            <MapPinIcon className="h-7 w-7 animate-pulse" />
+            <p>Taoyuan, Taiwan</p>
+          </div>
         </div>
+        <form className="mx-auto flex w-fit flex-col space-y-2">
+          <div className="flex space-x-2">
+            <input placeholder="Name" className="contactInput" type="text" />
+            <input placeholder="Email" className="contactInput" type="text" />
+          </div>
+          <input
+            placeholder="Subject"
+            className="contactInput w-full"
+            type="text"
+          />
+          <textarea placeholder="Message" rows={5} className="contactInput" />
+          <button
+            type="submit"
+            className="rounded bg-blue-600 px-4 py-2 font-semibold text-black"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
